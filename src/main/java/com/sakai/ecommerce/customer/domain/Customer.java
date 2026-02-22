e237ab100ebac44e0854d8331180bd17c776e8e0
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:25:45 -0300

    feat(customer): implement Customer aggregate root and repository interface

--- src/main/java/com/sakai/ecommerce/customer/domain/Customer.java
+++ src/main/java/com/sakai/ecommerce/customer/domain/Customer.java
@@ -1,4 +1,46 @@
 package com.sakai.ecommerce.customer.domain;
 
-public class Customer {
+import com.sakai.ecommerce.customer.domain.events.CreatedCustomerEvent;
+import com.sakai.ecommerce.shared.core.AggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Customer extends AggregateRoot<UUID> {
+    private String name;
+    private String lastName;
+    private LocalDate birthDate;
+    private String document;
+
+    @Embedded
+    private ContactInfo contactInfo;
+
+    @Embedded
+    private Address address;
+
+    public Customer(String document, String name, String lastName, LocalDate birthDate, ContactInfo contactInfo, Address address) {
+        if(id == null) id = UUID.randomUUID();
+        if(document == null || document.isBlank()) throw new IllegalArgumentException("CPF obrigatório");
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Nome obrigatório");
+        if (lastName == null || lastName.isBlank()) throw new IllegalArgumentException("Sobrenome obrigatório");
+        if (birthDate == null || birthDate.isAfter(LocalDate.now())) throw new IllegalArgumentException("Data de nascimento inválida");
+        if (contactInfo == null) throw new IllegalArgumentException("Informações de contato obrigatório");
+        if (address == null) throw new IllegalArgumentException("Endereço obrigatório");
+
+        this.document = document;
+        this.name = name;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.contactInfo = contactInfo;
+        this.address = address;
+
+        this.registerEvent(new CreatedCustomerEvent(this.id));
+    }
 }
--- src/main/java/com/sakai/ecommerce/customer/domain/CustomerRepository.java
+++ src/main/java/com/sakai/ecommerce/customer/domain/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.sakai.ecommerce.customer.domain;
 
+import java.util.Optional;
+
 public interface CustomerRepository {
+    void save(Customer customer);
+    Optional<Customer> findByDocument(String document);
 }
