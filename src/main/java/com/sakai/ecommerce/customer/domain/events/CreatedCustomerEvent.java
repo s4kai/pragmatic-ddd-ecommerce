b27907e5cf4fb125e5363a576daa72b2668ad63d
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:25:50 -0300

    feat(customer): add CreatedCustomerEvent domain event

--- src/main/java/com/sakai/ecommerce/customer/domain/events/CreatedCustomerEvent.java
+++ src/main/java/com/sakai/ecommerce/customer/domain/events/CreatedCustomerEvent.java
@@ -1,4 +1,12 @@
 package com.sakai.ecommerce.customer.domain.events;
 
-public class CreatedCustomerEvent {
+import com.sakai.ecommerce.shared.core.DomainEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+public class CreatedCustomerEvent extends DomainEvent {
+    private UUID customerId;
 }
