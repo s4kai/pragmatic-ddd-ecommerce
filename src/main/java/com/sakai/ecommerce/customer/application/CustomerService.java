d5c4eb5e29da1f6d29c853e08b67a3f9e87c1ae0
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:25:59 -0300

    feat(customer): implement CustomerService application service

--- src/main/java/com/sakai/ecommerce/customer/application/CustomerService.java
+++ src/main/java/com/sakai/ecommerce/customer/application/CustomerService.java
@@ -1,4 +1,17 @@
 package com.sakai.ecommerce.customer.application;
 
+import com.sakai.ecommerce.customer.application.dto.CustomerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+    private final CreateCustomer createCustomer;
+
+    public UUID create(CustomerDTO customerDTO) {
+        return createCustomer.handle(customerDTO);
+    }
 }
