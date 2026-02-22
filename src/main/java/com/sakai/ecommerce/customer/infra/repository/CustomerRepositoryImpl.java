5b682fb5789d1cf3a9a94f548bb9e5fedbec99e6
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:26:04 -0300

    feat(customer): implement JPA repository infrastructure

--- src/main/java/com/sakai/ecommerce/customer/infra/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/sakai/ecommerce/customer/infra/repository/CustomerRepositoryImpl.java
@@ -1,4 +1,23 @@
-package com.sakai.ecommerce.customer.infra.controllers;
+package com.sakai.ecommerce.customer.infra.repository;
 
-public class CustomerRepositoryImpl {
+import com.sakai.ecommerce.customer.domain.Customer;
+import com.sakai.ecommerce.customer.domain.CustomerRepository;
+import com.sakai.ecommerce.customer.infra.repository.jpa.JpaCustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomerRepositoryImpl implements CustomerRepository {
+    private final JpaCustomerRepository jpaRepository;
+
+    public void save(Customer customer) {
+        jpaRepository.save(customer);
+    }
+
+    public Optional<Customer> findByDocument(String document) {
+        return jpaRepository.findByDocument(document);
+    }
 }
--- src/main/java/com/sakai/ecommerce/customer/infra/repository/jpa/JpaCustomerRepository.java
+++ src/main/java/com/sakai/ecommerce/customer/infra/repository/jpa/JpaCustomerRepository.java
@@ -1,10 +1,12 @@
-package com.sakai.ecommerce.customer.infra.repository;
+package com.sakai.ecommerce.customer.infra.repository.jpa;
 
 
 import com.sakai.ecommerce.customer.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaCustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByDocument(String document);
 }
