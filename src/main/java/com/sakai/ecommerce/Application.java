a2ad50aeeca92a5d3e49ae17ad875a12b26e54db
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:26:17 -0300

    feat: configure Spring Boot application with modulith support

--- src/main/java/com/sakai/ecommerce/Application.java
+++ src/main/java/com/sakai/ecommerce/Application.java
@@ -2,12 +2,13 @@ package com.sakai.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
-public class PragmaticDddEcommerceApplication {
+@Modulithic(sharedModules = {"com.sakai.ecommerce.shared"})
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(PragmaticDddEcommerceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
