6176bc92a8fbc3cc30b459c4b3a9040d9294b9c7
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:25:40 -0300

    feat(customer): add Address and ContactInfo value objects

--- src/main/java/com/sakai/ecommerce/customer/domain/Address.java
+++ src/main/java/com/sakai/ecommerce/customer/domain/Address.java
@@ -1,4 +1,18 @@
 package com.sakai.ecommerce.customer.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String zipCode;
 }
--- src/main/java/com/sakai/ecommerce/customer/domain/ContactInfo.java
+++ src/main/java/com/sakai/ecommerce/customer/domain/ContactInfo.java
@@ -1,4 +1,24 @@
 package com.sakai.ecommerce.customer.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
 public class ContactInfo {
+    private String email;
+    private String phone;
+
+    public ContactInfo(String email, String phone) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Email inválido");
+        }
+
+        if (phone == null || phone.isBlank()) {
+            throw new IllegalArgumentException("Telefone obrigatório");
+        }
+
+        this.email = email;
+        this.phone = phone;
+    }
 }
