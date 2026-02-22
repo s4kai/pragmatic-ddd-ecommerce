39ce33283c69adcb4e77bcf911e15724e406557d
Vinicius Sakai <vinicius.sakai@tuta.io>
2026-02-21 23:26:13 -0300

    feat(customer): implement CustomerController REST endpoint

--- src/main/java/com/sakai/ecommerce/customer/infra/controllers/CustomerController.java
+++ src/main/java/com/sakai/ecommerce/customer/infra/controllers/CustomerController.java
@@ -1,4 +1,30 @@
 package com.sakai.ecommerce.customer.infra.controllers;
 
+import com.sakai.ecommerce.customer.application.CustomerService;
+import com.sakai.ecommerce.customer.infra.requests.CreateCustomerRequest;
+import com.sakai.ecommerce.customer.infra.requests.mappers.CreateCustomerRequestMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
+    private final CreateCustomerRequestMapper createCustomerRequestMapper;
+
+    @PostMapping
+    public ResponseEntity<UUID> create(@RequestBody @Valid CreateCustomerRequest dto) {
+        var customerDTO = createCustomerRequestMapper.toDTO(dto);
+        var createdCustomerId = customerService.create(customerDTO);
+
+        return ResponseEntity
+                .created(URI.create("/customers/" + createdCustomerId))
+                .body(createdCustomerId);
+    }
 }
