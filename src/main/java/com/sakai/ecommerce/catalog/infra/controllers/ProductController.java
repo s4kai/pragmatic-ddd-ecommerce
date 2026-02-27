f92311126368fa1d1ec86ea4bf11d7f16abbc276
Vinicius Sakai <vinicius.sakai@protonmail.com>
2026-02-27 14:28:28 -0300

    feat(catalog): add update product endpoint to ProductController

--- src/main/java/com/sakai/ecommerce/catalog/infra/controllers/ProductController.java
+++ src/main/java/com/sakai/ecommerce/catalog/infra/controllers/ProductController.java
@@ -1,13 +1,12 @@
 package com.sakai.ecommerce.catalog.infra.controllers;
 
 import com.sakai.ecommerce.catalog.application.CreateProduct;
+import com.sakai.ecommerce.catalog.application.UpdateProduct;
 import com.sakai.ecommerce.catalog.infra.requests.CreateProductRequest;
+import com.sakai.ecommerce.catalog.infra.requests.UpdateProductRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -16,11 +15,21 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProductController {
     private final CreateProduct createProduct;
+    private final UpdateProduct updateProduct;
 
     @PostMapping
     public ResponseEntity<UUID> create(@ModelAttribute CreateProductRequest request) {
         return ResponseEntity.ok(
             createProduct.handle(request.toCommand())
         );
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Void> update(
+            @PathVariable UUID productId,
+            @ModelAttribute UpdateProductRequest request
+    ) {
+        updateProduct.handle(request.toCommand(productId));
+        return ResponseEntity.noContent().build();
     }
 }
