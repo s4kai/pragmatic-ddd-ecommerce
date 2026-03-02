379f13ae638c66cddd92081431920df9c6a5987e
Vinicius Sakai <vinicius.sakai@protonmail.com>
2026-03-02 16:11:39 -0300

    feat(catalog): add search method to ProductRepository

--- src/main/java/com/sakai/ecommerce/catalog/domain/ProductRepository.java
+++ src/main/java/com/sakai/ecommerce/catalog/domain/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.sakai.ecommerce.catalog.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository {
     UUID save(Product product);
     Optional<Product> findById(UUID id);
+    Page<Product> findByFilters(String name, String description, Pageable pageable);
 }
