28434d34b0d3efef6e162cb8fab8453cf9fa57e1
Vinicius Sakai <vinicius.sakai@protonmail.com>
2026-03-04 14:28:09 -0300

    feat: use mysql database and configure spring session

---
+++ src/main/java/com/sakai/ecommerce/shared/infra/config/SessionConfig.java
@@ -0,0 +1,9 @@
+package com.sakai.ecommerce.shared.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfig {
+}
